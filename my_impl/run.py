9ac6101fa0abf75d8764944797d6b96758df18cc
Miyake J Takuma <i.takuma.miyake@globis.co.jp>
2021-03-07 10:31:41 +0900

    step22 completed

--- my_impl/run.py
+++ my_impl/run.py
@@ -1,13 +1,12 @@
 import numpy as np
-from variable import *
+from utils import no_grad
 from functions import *
+from variable import Variable
 
 
-x = Variable(np.array(0.5))
-a = square(x)
-b = exp(a)
-y = square(b)
+a = Variable(np.array(3.0))
+b = Variable(np.array(2.0))
 
-y.grad = np.array(1.0)
-y.backward()
-print(x.grad)
+print(a - b)
+print(a / b)
+print(a ** 3)
